e8eeee724d320437fbf365aaf902e24069f2b98f
Rathod Venkatesh <140520174+Venkyrathodofficial@users.noreply.github.com>
2026-01-26 17:20:27 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login(user, password) {
-  if (user === "admin" && password === "admin123") {
+  if (user === "admin" && password === "admin2356") {
     return "Login success";
   }
   return "Invalid credentials";
